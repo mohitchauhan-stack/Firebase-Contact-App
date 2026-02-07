e4a430701d7d4abfe50a43f50bbf4f1ac149af13
Mohit Chauhan <mohitchauhanwrk@gmail.com>
2026-02-07 20:54:40 +0530

    added imgs to scr/assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import firebaseLogo from '../assets/logos_firebase.svg';
+
 const Navbar = () => {
   return (
     <div className="my-2 flex h-15 items-center justify-center gap-4 rounded-lg bg-neutral-900 text-xl font-medium">
-      <img src="public/logos_firebase.svg" alt="firebase_logo" />
+      <img src={firebaseLogo} alt="firebase_logo" />
       <h1>Firebase Contact App</h1>
     </div>
   );
