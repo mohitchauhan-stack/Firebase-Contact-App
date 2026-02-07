0a83d88e48f3eaa189d5efd3544e2ce669a2aaaf
Mohit Chauhan <mohitchauhanwrk@gmail.com>
2026-02-07 20:47:59 +0530

    changed the img url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div className="my-2 flex h-15 items-center justify-center gap-4 rounded-lg bg-neutral-900 text-xl font-medium">
-      <img src="/logos_firebase.svg" alt="firebase_logo" />
+      <img src="logos_firebase.svg" alt="firebase_logo" />
       <h1>Firebase Contact App</h1>
     </div>
   );
